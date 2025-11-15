26f564635a91e0e39ee5905b7ae87c2fb828321f
lowkeyCodee <moisesnugal24@gmail.com>
2025-11-15 18:07:52 +0800

    Initial tasks for http

---
+++ easy/t1.js
@@ -0,0 +1,29 @@
+// 1. Create a basic HTTP server
+
+// Respond with plain text: "Hello, world".
+
+// 2. Return JSON
+
+// When the user visits /user, return a JSON object.
+
+// 3. Route based on URL
+
+// Handle:
+
+// -> /
+
+// -> /about
+
+// -> /contact
+
+// 4. Send an HTML page
+
+// Serve an HTML string as the response.
+
+// 5. Read query parameters
+
+// Handle:
+// /greet?name=Moises → respond: "Hello Moises".
+
+
+
